82224b106f5bd9a165e06e31f2e52b33f71e494b
Gosia <lm.sprachen@gmail.com>
2024-09-12 13:21:05 +0200

    statistische und dynamische templates fields admin

--- filmy/settings.py
+++ filmy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v68lh054ns&-$bf(z6)m4z=5dh-vy%u#dpa4nxq3mm$of1y#zw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'filmy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
