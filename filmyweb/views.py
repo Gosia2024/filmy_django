57ebf367e75547621b0009088043904bd81f2c99
Gosia <lm.sprachen@gmail.com>
2024-09-12 18:37:06 +0200

    CRUD

--- filmyweb/views.py
+++ filmyweb/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Film
+from .forms import FilmForm
+
 
 
 # Create your views here.
@@ -9,3 +11,12 @@ def wszystkie_filmy(request):
     wszystkie = Film.objects.all()
 
     return render(request, 'filmy.html', {'filmy':wszystkie})
+
+
+def nowy_film(request):
+    form = FilmForm(request.POST or None, request.FILES or None)
+
+    if form.is_valid():
+        form.save()
+        
+    return render(request, 'nowy_film.html', {'form': form})
